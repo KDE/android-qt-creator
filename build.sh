9de8d88d14adf190b445a3c5bdf2d2ea9b9b5080
mingw-android <mingw.android@gmail.com>
2011-03-15 02:29:39 +0000

    More build fixes.

--- build.sh
+++ build.sh
@@ -7,17 +7,29 @@ if [ "$1" = "" -o "$2" = "" ]; then
 fi
 
 if [ "$2" = "release" ]; then
-	QTDIR=/c/Qt/4.7.2-Git-MinGW-ShXcR
+        QTDIR=C:/Qt/4.7.2-Git-MinGW-ShXcR
 else
-	QTDIR=/c/Qt/4.7.2-Git-MinGW-ShXcD
+        QTDIR=C:/Qt/4.7.2-Git-MinGW-ShXcD
 fi
 
 # Using C:\Qt\4.7.1 doesn't work, though I should make this check the host os first, and use string substitution.
 INCLUDE="-I$QTDIR/include"
 PATH=$QTDIR/bin:$PATH
+# Mixed paths don't work with qmake (i.e. arg0 is always used to find the directory and it's always a Windows path).
+echo $QTDIR/bin/qmake.exe "QT_PRIVATE_HEADERS=$QTDIR/private-headers/include" "QT_CONFIG=webkit $1 svg declarative" -spec $QTDIR/mkspecs/win32-g++ -r `dirname $0`/qtcreator.pro
 $QTDIR/bin/qmake.exe "QT_PRIVATE_HEADERS=$QTDIR/private-headers/include" "QT_CONFIG=webkit $1 svg declarative" -spec $QTDIR/mkspecs/win32-g++ -r `dirname $0`/qtcreator.pro
 
-make -j9 $1
+# Without this, make will not be able to translate relative paths
+# properly as it can't step beyond where / is mounted.
+if [ "$OSTYPE" = "msys" ]; then
+	MAKEDIR=`pwd -W`
+	MAKEFILE=$MAKEDIR/Makefile
+else
+	MAKEFILE=Makefile
+fi
+
+
+make -f $MAKEFILE -j9 $1
 while [ "$?" != "0" ]
 do
 	if [ -f /usr/break-make ]; then
@@ -25,7 +37,7 @@ do
 		rm -f /usr/break-make
 		exit 1
 	fi
-	make -j9 $1
+	make -f $MAKEFILE -j9 $1
 done
 
-`dirname $0`/copy-to-qt-dir.sh $2 $QTDIR
+# `dirname $0`/copy-to-qt-dir.sh $2 $QTDIR
