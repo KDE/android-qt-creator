0b168514ab5571fdbfdad8aa5fac8126f77f1a18
Ray Donnelly <mingw.android@laptop.com>
2011-06-07 00:11:28 +0100

    Comment back out the QSettings::setPath() calls

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -258,13 +258,13 @@ int main(int argc, char **argv)
     if (!settingsPath.isEmpty())
         QSettings::setPath(QSettings::IniFormat, QSettings::UserScope, settingsPath);
 
-    // Must be done before any QSettings class is created
+    // Must be done before any QSettings class is created - they were the defaults anyway.
 #if defined(Q_OS_MAC)
-    QSettings::setPath(QSettings::IniFormat, QSettings::UserScope,
-            QDir::homePath() + "/.config");
+//    QSettings::setPath(QSettings::IniFormat, QSettings::UserScope,
+//            QDir::homePath() + "/.config");
 #elif defined(Q_OS_LINUX)
-    QSettings::setPath(QSettings::IniFormat, QSettings::UserScope,
-            QCoreApplication::applicationDirPath()+QLatin1String(SHARE_PATH));
+//    QSettings::setPath(QSettings::IniFormat, QSettings::UserScope,
+//            QCoreApplication::applicationDirPath()+QLatin1String(SHARE_PATH));
 #endif
 
     // plugin manager takes control of this settings object
