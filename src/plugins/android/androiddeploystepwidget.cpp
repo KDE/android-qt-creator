3009739e733f9560fb805836d1b7d80c32bbaac9
Daniel Teske <daniel.teske@nokia.com>
2011-09-07 17:24:12 +0200

    BuildStepConfigWdiget::init was removed in 2cc7bea, adjust Android
    Change-Id: Ic017889ff412d2aa4330c8311838f3b0d9dc2501

--- src/plugins/android/androiddeploystepwidget.cpp
+++ src/plugins/android/androiddeploystepwidget.cpp
@@ -30,6 +30,17 @@ AndroidDeployStepWidget::AndroidDeployStepWidget(AndroidDeployStep *step) :
     m_step(step)
 {
     ui->setupUi(this);
+
+    ui->useLocalQtLibs->setChecked(m_step->useLocalQtLibs());
+    switch (m_step->deployAction()) {
+    case AndroidDeployStep::DeployLocal:
+        ui->deployQtLibs->setChecked(true);
+        break;
+    default:
+        ui->devicesQtLibs->setChecked(true);
+        break;
+    }
+
     connect(m_step, SIGNAL(resetDelopyAction()), SLOT(resetAction()));
     connect(ui->devicesQtLibs, SIGNAL(clicked()), SLOT(resetAction()));
     connect(ui->deployQtLibs, SIGNAL(clicked()), SLOT(setDeployLocalQtLibs()));
@@ -41,19 +52,6 @@ AndroidDeployStepWidget::AndroidDeployStepWidget(AndroidDeployStep *step) :
 AndroidDeployStepWidget::~AndroidDeployStepWidget()
 {
     delete ui;
-}
-
-void AndroidDeployStepWidget::init()
-{
-    ui->useLocalQtLibs->setChecked(m_step->useLocalQtLibs());
-    switch (m_step->deployAction()) {
-    case AndroidDeployStep::DeployLocal:
-        ui->deployQtLibs->setChecked(true);
-        break;
-    default:
-        ui->devicesQtLibs->setChecked(true);
-        break;
-    }
 }
 
 QString AndroidDeployStepWidget::displayName() const
