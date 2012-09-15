8b2934c367e92fd99a38dc67b76a4847e91c2f37
BogDan Vatra <bogdan@kde.org>
2012-09-15 14:49:13 +0300

    Add QMAKE_INCDIR_xxxxx to headers search paths
    BUG: 306699

--- src/plugins/qt4projectmanager/qt4nodes.cpp
+++ src/plugins/qt4projectmanager/qt4nodes.cpp
@@ -2149,6 +2149,13 @@ QStringList Qt4ProFileNode::includePaths(QtSupport::ProFileReader *reader) const
     }
 
     paths.append(reader->absolutePathValues(QLatin1String("INCLUDEPATH"), m_projectDir));
+    paths << m_readerExact->values(QLatin1String("QMAKE_INCDIR"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_X11"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_QT"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_OPENGL"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_OPENGL_ES1"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_EGL"))
+          << m_readerExact->values(QLatin1String("QMAKE_INCDIR_OPENVG"));
     // paths already contains moc dir and ui dir, due to corrrectly parsing uic.prf and moc.prf
     // except if those directories don't exist at the time of parsing
     // thus we add those directories manually (without checking for existence)
